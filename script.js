6f6402e0a7ea907239dbcdc3b080b0e1875f926f
Razon <rracsko@gmail.com>
2021-12-13 20:05:29 +0100

    second version with reset and resize

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
 
-
-
 ///////////////////////////////////////////////////0
 /////////////////////SETTING UP GRID//////////////
 
 const etchBox = document.querySelector(".etchbox");
 const windowSize = 512;
-var div;
-var size = 32;
-var box;
+let div;
+let size = 16;
+let box;
+let sketchbox;
 
-for (j = 0; j < size; j++) {
-    for (i = 1; i <= size; i++) {
-        div = document.createElement("div");
-        div.style.width = windowSize/size + "px";
-        div.style.height = windowSize/size + "px";
-        div.setAttribute("class", "sketch");
-        div.setAttribute("id", i + j * size);
-        etchBox.appendChild(div);
+
+const resetButton = document.querySelector("#reset");
+let slider = document.querySelector("#gridsize") ;
+
+resetButton.addEventListener("click", resetGrid);
+
+function resetGrid (e) {
+    size = slider.value;
+    
+    while (etchBox.firstChild){
+        etchBox.removeChild(etchBox.lastChild);
     }
+    
+   
+    setUp(size);
+    
 }
 
-const sketchBox = document.querySelectorAll(".sketch");
+
+function setUp (size) {
+    for (j = 0; j < size; j++) {
+        for (i = 1; i <= size; i++) {
+            div = document.createElement("div");
+            div.style.width = windowSize/size + "px";
+            div.style.height = windowSize/size + "px";
+            div.setAttribute("class", "sketch");
+            div.setAttribute("id", i + j * size);
+            etchBox.appendChild(div);
+        }
+    }
+    sketchBox = document.querySelectorAll(".sketch");
+    sketchBox.forEach(box => {
+        box.addEventListener("mouseover", fillBox);
+    });
+}
+
+
 
 ////////////////////////////////////////////////
 //////////////COLORING///////////////////////
 
 
-sketchBox.forEach(box => {
-    box.addEventListener("mouseover", fillBox);
-});
 
 function fillBox(e) {
     e.srcElement.setAttribute("class", "colored");
 
 }
 
+setUp(size);
 
 ////////////////////////////////////////
 /////////////RESETTING GRID//////////
 
 
 
-const resetButton = document.querySelector("#reset");
 
-resetButton.addEventListener("click", resetGrid);
-
-function resetGrid (e) {
-    sketchBox.forEach(box => {
-        box.removeAttribute("class", "colored");
-    })
-}
